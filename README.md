cb9b75499ee43f7bc5c607987c3937ebc5351a53
vishwa-oshad <83592310+vishwa-oshad@users.noreply.github.com>
2025-11-17 08:32:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lara-react-crud
+
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\In;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render();
+    }
+}
